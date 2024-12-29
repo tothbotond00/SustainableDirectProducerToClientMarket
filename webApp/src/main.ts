aea441905512ee94651312e8cae42659963110de
Gábor <beresgabo2002@gmail.com>
2024-12-29 16:36:14 +0100

    deploy settings

--- webApp/src/main.ts
+++ webApp/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app/app.module';
 
 const providers = [
   { provide: 'BASE_URL', useValue: 'https://localhost:7100/' }
+  // { provide: 'BASE_URL', useValue: 'https://sdptcm-api.azurewebsites.net/' }
 ]
 
 platformBrowserDynamic(providers).bootstrapModule(AppModule, {
